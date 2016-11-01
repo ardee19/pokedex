80ee6052ae24c8ede2168da5878463e6c1bc3bb8
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2016-10-31 20:45:13 -0700

    Make Collection View Cell

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Roy Dimayuga on 10/31/16.
+//  Copyright © 2016 www.WeAreSuperAwesome.net. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder:NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon:Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,29 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Roy Dimayuga on 10/31/16.
+//  Copyright © 2016 www.WeAreSuperAwesome.net. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+}
